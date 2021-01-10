8a84d44d469704b2be5cd4c17d99166b0a0a4259
GuschinS <guschins@gmail.com>
2021-01-10 18:43:51 +0300

    edit id

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
     # In a production app, store this instead in KeyVault or an environment variable
     # TODO: Enter your client secret from Azure AD below
-    CLIENT_SECRET = "0106efe3-e6b0-4942-aee1-4aab2e846c81" 
+    CLIENT_SECRET = "1UcW7V4Nu78g_0_.~BPH2_t7T1-v-CUsFu"
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
@@ -11,8 +11,8 @@ class Config(object):
 
     # TODO: Enter the redirect path you want to use for OAuth requests
     #   Note that this will be the end of the URI entered back in Azure AD
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL, 
-        # which must match your app's redirect_uri set in AAD
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL,
+    # which must match your app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
